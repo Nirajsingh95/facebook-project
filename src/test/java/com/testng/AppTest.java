9508774bc0fd41fda29eb3baf0a320bf4a551f3f
sasigowda <32127896+sasigowda@users.noreply.github.com>
2022-11-19 18:42:32 +0530

    up

--- src/test/java/com/testng/AppTest.java
+++ src/test/java/com/testng/AppTest.java
@@ -79,9 +79,9 @@ public class AppTest {
 
         // to take screenshot
         ScreenShotUtil.takeScreenShot(webdriver);
-    */
+    
     }
-
+    */
     /**
      * click on facebook home
      */
